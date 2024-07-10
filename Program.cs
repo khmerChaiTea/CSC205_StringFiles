2c617556f019e40612d0d599ba1ed5a7e33231e6
pchai <prempreychhay@gmail.com>
2024-07-10 13:47:37 -0700

    Run-timer Errors

--- Program.cs
+++ Program.cs
@@ -6,25 +6,25 @@ using System.Threading.Tasks;
 
 namespace CSC205_StringFiles
 {
-    public class Program
+    namespace ThinkSharp
     {
-        public static void Main(string[] args)
+        public class BadString
         {
-            string s = "Visual C#";
-            System.Console.WriteLine(s.Substring(7, 2));
-            // Output: "C#"
-
-            System.Console.WriteLine(s.Replace("C#", "Basic"));
-            // Output: "Visual Basic Express"
-
-            System.Console.WriteLine(s.ToUpper());
-            // Output: "VISUAL C#"
-
-            System.Console.WriteLine(s.Contains("C#"));
-            //Output: True
-
-            System.Console.WriteLine(s.Insert(0, "I like "));
-            //Output: I like Visual C#
+            public static void Main(string[] args)
+            {
+                ProcessWord("banana");
+            }
+            public static void ProcessWord(string s)
+            {
+                char c = GetLastLetter(s);
+                Console.WriteLine(c);
+            }
+            public static char GetLastLetter(string s)
+            {
+                int index = s.Length; // WRONG!
+                char c = s[index];
+                return c;
+            }
         }
     }
 }
