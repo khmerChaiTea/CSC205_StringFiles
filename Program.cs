1d110a7ddd1f49b14791cae08c8c3535769244e2
pchai <prempreychhay@gmail.com>
2024-07-10 13:32:40 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC205_StringFiles
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = "abcdefg";
+
+            for (int i = 0; i < str.Length; i += 2) 
+            {
+                Console.WriteLine(str[i]);
+            }
+            Console.WriteLine();
+
+            foreach (char c in str)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
+    }
+}
